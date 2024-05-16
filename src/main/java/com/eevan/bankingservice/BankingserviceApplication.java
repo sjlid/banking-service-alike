afe2112cfbfaab90a65f5e255624a0196342d743
Ivan <senjohn.0000@gmail.com>
2024-05-17 02:20:41 +0300

    add project

---
+++ src/main/java/com/eevan/bankingservice/BankingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.eevan.bankingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingserviceApplication.class, args);
+	}
+
+}
