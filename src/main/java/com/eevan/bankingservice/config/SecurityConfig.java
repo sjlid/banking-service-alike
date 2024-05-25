8cd98e803b2fef859a21d864e2c106120862c68f
eevan <senjohn.0000@gmail.com>
2024-05-25 20:39:19 +0300

    add security (wip)

--- src/main/java/com/eevan/bankingservice/config/SecurityConfig.java
+++ src/main/java/com/eevan/bankingservice/config/SecurityConfig.java
@@ -1,22 +1,30 @@
 package com.eevan.bankingservice.config;
 
-import com.eevan.bankingservice.security.AuthProviderImpl;
+import com.eevan.bankingservice.services.EmployeeDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final EmployeeDetailsService employeeDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(EmployeeDetailsService employeeDetailsService) {
+        this.employeeDetailsService = employeeDetailsService;
     }
 
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(employeeDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
