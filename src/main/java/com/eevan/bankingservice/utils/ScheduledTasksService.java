0480805e373d56d2e2ff633361279a04c00741c2
eevan <senjohn.0000@gmail.com>
2024-07-03 16:32:49 +0300

    added async to job

--- src/main/java/com/eevan/bankingservice/utils/ScheduledTasksService.java
+++ src/main/java/com/eevan/bankingservice/utils/ScheduledTasksService.java
@@ -2,6 +2,7 @@ package com.eevan.bankingservice.utils;
 
 import com.eevan.bankingservice.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ public class ScheduledTasksService {
     @Autowired
     private ClientService clientService;
 
+    @Async
     @Scheduled(fixedRate = 60000)
     public void updateClientBalances() {
         clientService.updateBalance();
